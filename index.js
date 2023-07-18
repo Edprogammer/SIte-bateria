272cd215b9b9a484cb5a237af05031904f736373
Eduardo dos Santos Maia <124004467+Edprogammer@users.noreply.github.com>
2023-07-17 22:57:05 -0300

    sobre

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
  function redirectToFacebook() {
       window.open('https://www.facebook.com/profile.php?id=100089364951522', '_blank');
     }
+function redirectToAbout1() {
+window.open('http://127.0.0.1:5500/p%C3%A1ginas/pag1.html', '_blank');
+}
+function redirectToAbout2() {
+  window.open('http://127.0.0.1:5500/p%C3%A1ginas/pag2.html', '_blank');
+  }
+  function redirectToAbout3() {
+    window.open('http://127.0.0.1:5500/p%C3%A1ginas/pag3.html', '_blank');
+    }
